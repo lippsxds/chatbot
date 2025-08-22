ba8d8d0c90faf475c896661f595470f8ae01095d
lippsxds <lippsxd@gmail.com>
2025-08-22 17:09:26 +0530

    Update owner.py

--- handlers/owner.py
+++ handlers/owner.py
@@ -1,1 +1,13 @@
+from pyrogram import filters
+from config import OWNER_ID
+from utils.broadcast import broadcast_text
+
+def register_handlers(app):
+    @app.on_message(filters.private & filters.command("broadcast") & filters.user(OWNER_ID))
+    async def broadcast_command(client, message):
+        text = message.text.split(" ", 1)
+        if len(text) < 2:
+            await message.reply_text("Usage: /broadcast Your message here")
+            return
+        await broadcast_text(client, message, text[1])
 
